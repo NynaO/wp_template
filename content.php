dcbfbd59d4e330e7215b9cbfdb085e4cad82ff3b
NynaO <njbraesch@gmail.com>
2016-05-11 11:52:27 +0200

    11/05/2016
    Updated style for Masonry grid:
    - fixed font size
    - added gutter and bottom margin
    - grid component centered
    
    Updated component file:
    - fixed <div> and <article> tags: for all articles display, the masonry brick now includes the article tag instead of being embedded inside the article (the Masonry lib thus considered that the article was the actual masonry brick, which led to minor display bugs)

--- content.php
+++ content.php
@@ -6,8 +6,8 @@
  *
  * @package faux-cale
  */?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <?php if ( is_single() ) { /*single post, no need to implement Masonry bricks*/?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' );?>
 	</header><!-- .entry-header -->
@@ -26,10 +26,12 @@
 	<footer class="entry-footer">
 		<?php faux_cale_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
 <?php } else {
 /*all the posts: we use Masonry bricks to display them all*/
 ?>
 <div class="brick">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );?>
 		</header><!-- .entry-header -->
@@ -43,6 +45,6 @@
 					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'faux-cale' ), array( 'span' => array( 'class' => array() ) ) ),
 					the_title( '<span class="screen-reader-text">"', '"</span>', false )) );}?>
 		</div><!-- .entry-content -->
+</article><!-- #post-## -->
 </div>
 <?php }?>
-</article><!-- #post-## -->
